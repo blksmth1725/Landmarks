d9607b799bc9638a65d586decb45011ae89f4d29
Christian Sheen <csheen1725@gmail.com>
2021-08-29 14:07:16 -0400

    Created Badge && BadgeSymbol

---
+++ Shared/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Christian Sheen on 8/29/21.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+     BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
