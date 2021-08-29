de657dfd14a0f863e9f5caed9c51a9515ee4ea6c
Christian Sheen <csheen1725@gmail.com>
2021-08-28 20:49:46 -0400

    LandmarkList: Added toggle button which filters the favorite parks into a separate array and displays a synthesized list

--- Shared/Views/LandmarkRow.swift
+++ Shared/Views/LandmarkRow.swift
@@ -17,6 +17,11 @@ struct LandmarkRow: View {
         Text(landmark.name)
             
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -24,8 +29,8 @@ struct LandmarkRow: View {
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
+        LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
-        LandmarkRow(landmark: landmarks[2])
         }
             .previewLayout(.fixed(width: 300, height: 70))
     }
