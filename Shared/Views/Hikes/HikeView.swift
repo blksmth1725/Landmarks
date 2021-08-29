11c1a5dc9d0394caf47fd1abcdbfdc0f669252ac
Christian Sheen <csheen1725@gmail.com>
2021-08-29 18:42:21 -0400

    Adding animation to through toggle method on chevron icon

--- Shared/Views/Hikes/HikeView.swift
+++ Shared/Views/Hikes/HikeView.swift
@@ -1,41 +1,46 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
                     .animation(nil)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation {
+                        self.showDetail.toggle()
+                    }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
+                    
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
             }
